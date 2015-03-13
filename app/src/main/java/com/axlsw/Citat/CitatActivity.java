d5ae8cde8619adbd5b81b18a3f548c88e1448480
DenK91 <den.k9188@gmail.com>
2015-03-13 22:59:39 +0300

    add citat activity layout

--- app/src/main/java/com/axlsw/Citat/CitatActivity.java
+++ app/src/main/java/com/axlsw/Citat/CitatActivity.java
@@ -2,31 +2,49 @@ package com.axlsw.Citat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 /**
- *
+ * Main Activity for Citat app.
  */
 public class CitatActivity extends Activity {
 
-    AdView mAdView;
+    private static AdView mAdView;
+    private LinearLayout mAdMobContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citat);
 
-        mAdView = new AdView(this);
-        mAdView.setAdUnitId("ca-app-pub-4867342099234785/3519008168");
-        mAdView.setAdSize(AdSize.BANNER);
-        LinearLayout container = (LinearLayout) findViewById(R.id.admobContainer);
-        container.addView(mAdView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+        initAdMob();
+    }
+
+    /**
+     * Init adView.
+     */
+    void initAdMob() {
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder().build();
+        mAdView.loadAd(request);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mAdView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                mAdView.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
